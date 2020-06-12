f480561437b5e67335a4ba01ce4c08f5759ce5d9
Barrnet <barrnet@videogamezone.eu>
2020-06-12 22:58:38 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 //Api Telegram, replace "XXXXXXX:XXXXXXXXXXXXXXXXXXXXX" with your API KEY
-define('BOT_TOKEN', '');
+define('BOT_TOKEN', 'XXXXXXX:XXXXXXXXXXXXXXXXXXXXX');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 //Bot id, change with your bot id
-define('BOT_ID', '@');
+define('BOT_ID', '@botname');
 define('REF_AMZ','');
 ?>
